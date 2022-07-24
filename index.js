7076b4bc3450a778c00eae317a4f7149443a3425
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-24 01:49:11 +0000

    implemented sum function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  return toN + sum(fromN, toN - 1);
 }
+
+console.log(sum(3, 7));
 
 module.exports = sum;
